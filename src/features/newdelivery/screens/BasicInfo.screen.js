3f160fa19ec8f2fb3524bf94093435ae0311e4ad
Jean Snyman <jeansn@tfg.co.za>
2022-01-20 18:09:22 +0200

    added react native paper dropdown

--- src/features/newdelivery/screens/BasicInfo.screen.js
+++ src/features/newdelivery/screens/BasicInfo.screen.js
@@ -1,83 +1,144 @@
 import React, { useState } from "react";
 import { ScrollView, View } from "react-native";
-import { TextInput, Card, Button } from "react-native-paper";
-import { Picker } from "@react-native-picker/picker";
+import {
+  TextInput,
+  Card,
+  Button,
+  Provider,
+  DefaultTheme,
+} from "react-native-paper";
+import DropDown from "react-native-paper-dropdown";
 
 import styled from "styled-components/native";
 
 const Input = styled(TextInput)`
-  margin-bottom: 40px;
+  margin-bottom: 10px;
+`;
+
+const DropDownSelector = styled.View`
+  margin-bottom: 10px;
 `;
 
 export const BasicInfo = ({ navigation }) => {
-  const [country, setCountry] = useState("Unknown");
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [gender, setGender] = useState("");
+
+  const genderList = [
+    {
+      label: "Male",
+      value: "male",
+    },
+    {
+      label: "Female",
+      value: "female",
+    },
+    {
+      label: "Others",
+      value: "others",
+    },
+    {
+      label: "Others2",
+      value: "others2",
+    },
+    {
+      label: "Others3",
+      value: "others3",
+    },
+  ];
 
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      keyboardShouldPersistTaps="handled"
-    >
-      <Card
-        style={{
-          flex: 1,
-          paddingHorizontal: 10,
-          paddingVertical: 20,
-        }}
+    <Provider theme={DefaultTheme}>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
       >
-        <View style={{ flex: 1, justifyContent: "space-between" }}>
-          <View>
-            <Input
-              label="Order Number"
-              mode="outlined"
-              selectionColor="#013D73"
-              activeUnderlineColor="#013D73"
-              activeOutlineColor="#013D73"
-              keyboardType="numeric"
-            />
-            <Input
-              label="Account Number"
-              mode="outlined"
-              selectionColor="#013D73"
-              activeUnderlineColor="#013D73"
-              activeOutlineColor="#013D73"
-              keyboardType="numeric"
-            />
-            <Input
-              label="Pedometer"
-              mode="outlined"
-              selectionColor="#013D73"
-              activeUnderlineColor="#013D73"
-              activeOutlineColor="#013D73"
-              keyboardType="numeric"
-            />
-            <Picker
-              selectedValue={country}
-              onValueChange={(value) => setCountry(value)}
-              mode="dropdown" // Android only
-            >
-              <Picker.Item
-                label="Please select your retail site"
-                value="Unknown"
+        <Card
+          style={{
+            flex: 1,
+            paddingHorizontal: 10,
+            paddingVertical: 20,
+          }}
+        >
+          <View style={{ flex: 1, justifyContent: "space-between" }}>
+            <View>
+              <Input
+                label="Order Number"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
               />
-              <Picker.Item label="Australia" value="Australia" />
-              <Picker.Item label="Belgium" value="Belgium" />
-              <Picker.Item label="Canada" value="Canada" />
-              <Picker.Item label="India" value="India" />
-              <Picker.Item label="Japan" value="Japan" />
-            </Picker>
+              <DropDownSelector>
+                <DropDown
+                  label={"Select Site Name"}
+                  mode={"outlined"}
+                  visible={showDropDown}
+                  showDropDown={() => setShowDropDown(true)}
+                  onDismiss={() => setShowDropDown(false)}
+                  value={gender}
+                  setValue={setGender}
+                  list={genderList}
+                  dropDownItemSelectedTextStyle={{
+                    color: "#013D73",
+                  }}
+                />
+              </DropDownSelector>
+              <Input
+                label="Pedometer"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
+              />
+              <Input
+                label="Account Number"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
+              />
+
+              <Input
+                label="Account Number"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
+              />
+              <Input
+                label="Account Number"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
+              />
+              <Input
+                label="Account Number"
+                mode="outlined"
+                selectionColor="#013D73"
+                activeUnderlineColor="#013D73"
+                activeOutlineColor="#013D73"
+                keyboardType="numeric"
+              />
+            </View>
+            <View>
+              <Button
+                icon="send"
+                mode="contained"
+                onPress={() => {}}
+                contentStyle={{ height: 60, backgroundColor: "#013D73" }}
+              >
+                Continue
+              </Button>
+            </View>
           </View>
-          <View>
-            <Button
-              icon="send"
-              mode="contained"
-              onPress={() => {}}
-              contentStyle={{ height: 60, backgroundColor: "#013D73" }}
-            >
-              Continue
-            </Button>
-          </View>
-        </View>
-      </Card>
-    </ScrollView>
+        </Card>
+      </ScrollView>
+    </Provider>
   );
 };
